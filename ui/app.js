b7dec7a7160adc16665b4cdc506be406e853e655
chanwook <chanwook.god@gmail.com>
2013-12-20 12:20:53 +0900

    view 분리

--- ui/app.js
+++ ui/app.js
@@ -1,34 +1,9 @@
-//-- View --//
-var View = function() {
-	//Model을 Observe한다는 것을 목표로 하지만, 아직은 구현 못함. how? 
-	this.render = function(model, templateUrl, templateKey){
-		$.get( templateUrl, function( template ) {
-	  		console.log("load template html: ", template);
-
-			// Compile 
-			var compiled = dust.compile(template, templateKey);
-			console.log("compiled: ", compiled);
-
-			// Load
-			dust.loadSource(compiled);
-
-			// Rendering
-			console.log("responseText: " , model.responseText);
-			dust.render(templateKey, JSON.parse(model.responseText) , function(err, out){
-				console.log("final(success): ", out);
-				console.log("final(err): ", err);
-				$("#table_content").html(out);
-			});
-		});
-	};
-};
-//-- View --//
-
 // -- Model -- //
 var ProductModel = function() {
 	this.get = function(){
-		var modelUrl = "https://raw.github.com/chanwookpark/singlepageapp/master/ui/product-model.json";
-		var modelJson = $.getJSON(modelUrl);
+		var modelJson = 
+			$.getJSON("https://raw.github.com/chanwookpark/singlepageapp/master/ui/product-model.json");
+		
 		console.log("Model: ", modelJson);
 		return modelJson;
 	};
@@ -40,8 +15,9 @@ var ViewEventLogic = function() {
 	this.init = function() {
 		//json 받아.. 
 		var model = productModel.get();
-		
-		productView.render(model, templateUrl, templateKey);
+		productView.render(model, function(err, out){
+			$("#table_content").html(out);
+		});
 	}
 }
 // -- Event Logic -- //
---
+++ ui/view.js
@@ -0,0 +1,29 @@
+//-- View --//
+var View = function(templateKey, templateUrl) {
+	var templateKey = templateKey;
+	var templateUrl = templateUrl;
+
+	//Model을 Observe한다는 것을 목표로 하지만, 아직은 구현 못함. how? 
+	this.render = function(model, renderingCallback){
+		$.get(templateUrl, function( template ) {
+	  		console.log("load template html: ", template);
+
+			// Compile 
+			var compiled = dust.compile(template, templateKey);
+			console.log("compiled: ", compiled);
+
+			// Load
+			dust.loadSource(compiled);
+
+			// Rendering
+			console.log("responseText: " , model.responseText);
+			dust.render(templateKey, JSON.parse(model.responseText) , function(err, out){
+				console.log("final(success): ", out);
+				console.log("final(err): ", err);
+					
+				renderingCallback(err, out);
+			});
+		});
+	};
+};
+//-- View --//
