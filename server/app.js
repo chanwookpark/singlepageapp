67d25c1f266a9abc2debe824af84a1b768781e8b
chanwook <chanwook.god@gmail.com>
2013-12-26 07:55:32 +0900

    서버 연결 모듈 개발 (앞선 커밋과 같은 작업!)

---
+++ server/app.js
@@ -0,0 +1,18 @@
+
+
+var express = require('express');
+var server = express();
+
+var fs = require('fs');
+
+
+
+server.get('/products', function(req, res){
+	var data = fs.readFileSync('data.json', 'utf-8');
+	console.log('load data ', data);
+	res.setHeader('Content-Type', 'application/json');
+	res.send(data);
+});
+
+server.listen(3000);
+console.log('Start HTTP Server(listen on 3000 port');
