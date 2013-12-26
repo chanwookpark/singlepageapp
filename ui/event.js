5aee5258462f56697351e7737def27b2afc32b34
chanwook <chanwook.god@gmail.com>
2013-12-26 13:50:12 +0900

    전역변수 오염이 있는 Model 관련 코드를 수정
    임시로 require() 기능 구현..

--- ui/event.js
+++ ui/event.js
@@ -2,8 +2,7 @@
 var ViewEvent = function() {
 	"use strict";
 
-	this.init = function() {
-
+	this.init = function(productModel, productView) {
 		var model = productModel.load();
 		productView.render(model, function(err, out){
 			$("#table_content").html(out);
