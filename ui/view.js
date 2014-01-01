9663a5390385c4551427685eec1c505ebbc34a66
chanwook <chanwook.god@gmail.com>
2014-01-01 14:36:35 +0900

    소소한 코드 수정

--- ui/view.js
+++ ui/view.js
@@ -6,22 +6,22 @@ define(function(){
 	};
 
 	//TODO Model을 Observe한다는 것을 목표로 하지만, 아직은 구현 못함
-	exports.render = function(templateKey, templateUrl, model, renderingCallback){
+	exports.render = function(templateKey, templateUrl, json, renderingCallback){
 		$.get(templateUrl, function( template ) {
-	  		console.log("load template html: ", template);
+	  		console.log(">> load template html: ", template);
 
 			// Compile 
 			var compiled = dust.compile(template, templateKey);
-			console.log("compiled: ", compiled);
+			console.log(">> compiled: ", compiled);
 
 			// Load
 			dust.loadSource(compiled);
 
 			// Rendering
-			console.log("responseText: " , model.responseText);
-			dust.render(templateKey, JSON.parse(model.responseText) , function(err, out){
-				console.log("final(success): ", out);
-				console.log("final(err): ", err);
+			console.log(">> JSON: " , json.responseText);
+			dust.render(templateKey, JSON.parse(json.responseText) , function(err, out){
+				console.log(">> final(success): ", out);
+				console.log(">> final(err): ", err);
 					
 				renderingCallback(err, out);
 			});
